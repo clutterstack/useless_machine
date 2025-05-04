90008f2425897310cbfa149189eec5f8f44ef7a6
Chris Nicoll <5599856+clutterstack@users.noreply.github.com>
2025-05-04 00:09:07 -0400

    add Tidewave plug

--- lib/useless_machine_web/endpoint.ex
+++ lib/useless_machine_web/endpoint.ex
@@ -33,6 +33,11 @@ defmodule UselessMachineWeb.Endpoint do
     gzip: false,
     only: UselessMachineWeb.static_paths()
 
+
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
